b2841333c43b1dd7afab611523d6ff4a05dc1d97
Kris <kris_s2@outlook.com>
2019-04-29 12:55:16 -0700

    Add project files.

---
+++ WebAppSignalR/Pages/SignalR.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebAppSignalR.Pages
+{
+    public class SignalRModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+
+    }
+}
---
+++ WebAppSignalR/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿var conn = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+conn.on("ReceiveMessage", (user, message) => {
+    const rec_msg = user + ":" + message;
+    const li = document.createElement("li");
+    li.textContent = rec_msg;
+    document.getElementById("messageList").appendChild(li);
+});
+
+conn.start().catch(err => console.error(err.toString()));
+
+
+document.getElementById("sendMessage").addEventListener("click", event => {
+
+    const user = document.getElementById("userName").value;
+    const message = document.getElementById("userMessage").value;
+
+    conn.invoke("SendMessage", user, message).catch(err => console.error(err.toString()));
+    event.preventDefault();
+});
