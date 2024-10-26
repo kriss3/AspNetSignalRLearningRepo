2a18bcd39a402841d1dd2c7f2357fe785dc7b162
Kris <kris_s2@outlook.com>
2024-10-26 12:43:46 -0700

    kata: add handler to read api url from appsettings, also adding handing in case this fails;

--- ConAppSignalApp.Client/Models/AppSettngsModel.cs
+++ ConAppSignalApp.Client/Models/AppSettngsModel.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConAppSignalApp.Client.Models;
-internal class AppSettngsModel
+﻿namespace ConAppSignalApp.Client.Models;
+public record AppSettingsModel 
 {
-}
+	public string? HubUrl { get; init; }
+};
