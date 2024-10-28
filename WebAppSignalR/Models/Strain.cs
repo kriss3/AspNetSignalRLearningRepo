10b88310318b33f04a5e79021bf66f43a457785e
Kris <kris_s2@outlook.com>
2024-10-27 23:57:08 -0700

    kata: adding models to API for Strians. Metrc Connect.

---
+++ WebAppSignalR/Models/Strain.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAppSignalR.Models;
+
+public record Strain(
+		int Id,
+		Option<string> Name,
+		Option<string> TestingStatus,
+		Option<float> ThcLevel,
+		Option<float> CbdLevel,
+		float IndicaPercentage,
+		float SativaPercentage,
+		bool IsUsed,
+		Option<string> Genetics
+	);
