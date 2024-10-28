8a5d57ad3277bf9d8db664134e22a1b1b9252a0f
Kris <kriss3@users.noreply.github.com>
2024-10-27 23:58:16 -0700

    Kata: Adding model for Strains. (#9)
    Adding models to API for Strains. Metrc Connect.

---
+++ WebAppSignalR/Models/PaginatedResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace WebAppSignalR.Models;
+
+public record PaginatedResponse<T>(
+	List<T> Data,
+	int Total,
+	int TotalRecords,
+	int PageSize,
+	int RecordsOnPage,
+	int Page,
+	int CurrentPage,
+	int TotalPages
+);
