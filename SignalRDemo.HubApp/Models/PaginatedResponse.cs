cb8c0c3c66812d12c6b5911724c547bb679ac5a5
Kris <kris_s2@outlook.com>
2024-10-28 23:36:38 -0700

    kata: Adding Models and scaffolidng inteface for Get operations; Adding Functional lib.

---
+++ SignalRDemo.HubApp/Models/PaginatedResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SignalRDemo.HubApp.Models;
+
+public record PaginatedResponse<T>(
+	List<T> Data,
+	int Total,
+	int TotalRecords,
+	int PageSize,
+	int RecordsOnPage,
+	int Page,
+	int CurrentPage,
+	int TotalPages
+);
